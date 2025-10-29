53f822d6e06b09ff6a31b667411283e572345521
jinseohyun1228 <15roa1121@gmail.com>
2025-10-29 16:08:10 +0900

    9.5일차  DP + 우선순위큐

--- day09-priority-queue-and-DP/1202 보석 도둑.py
+++ day09-priority-queue-and-DP/1202 보석 도둑.py
@@ -1,1 +1,24 @@
-import sys.stdin.readline
+import sys
+import heapq
+from collections import deque
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+
+gem = [ [int(x) for x in input().split()] for _ in range(n)]
+bag = [int(input()) for _ in range(k)]
+
+gem.sort()
+gem = deque(gem)
+bag.sort()
+
+pq = []
+result = 0
+for i in bag:
+    while gem and gem[0][0] <= i :
+        heapq.heappush(pq,-gem[0][1])
+        gem.popleft()
+    if pq:
+        result -= heapq.heappop(pq)
+
+print(result)
